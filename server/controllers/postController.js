ae454af4e814cd7c7ce2c0282eff84f495511374
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-05-14 17:47:46 +0530

    Complete the likes route in posts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,28 +1,28 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const { supabase } = require("../config/db");
+const { supabase, adminAuthClient } = require("../config/db");
 
 exports.getPost = asyncHandler(async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-  
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-    const { data: postData, error: postError } = await supabase
-      .from("posts")
-      .select()
-      .eq("post_id", req.params.postId);
-    if (postError) {
-      return res.status(400).json({ error: postError.message });
-    }
-    res.status(200).json(postData);
-  });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  const { data: postData, error: postError } = await supabase
+    .from("posts")
+    .select()
+    .eq("post_id", req.params.postId);
+  if (postError) {
+    return res.status(400).json({ error: postError.message });
+  }
+  res.status(200).json(postData);
+});
 
 exports.createPost = [
   body("caption").notEmpty().withMessage("Caption cannot be empty"),
@@ -51,6 +51,12 @@ exports.createPost = [
       .select();
     if (postError) {
       return res.status(400).json({ error: postError.message });
+    }
+    const { error } = await adminAuthClient.updateUserById(user.id, {
+      user_metadata: { posts: user.user_metadata.posts + 1 },
+    });
+    if (error) {
+      return res.status(400).json({ error: error.message });
     }
     res.status(200).json({ post: postData });
   }),
@@ -109,5 +115,139 @@ exports.deletePost = asyncHandler(async (req, res) => {
   if (postError) {
     return res.status(400).json({ error: postError.message });
   }
+  const { error } = await adminAuthClient.updateUserById(user.id, {
+    user_metadata: { posts: user.user_metadata.posts - 1 },
+  });
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
   res.status(200).json(postData);
+});
+
+/**********************************
+ *  Likes Controller
+ * ********************************/
+
+exports.likePost = asyncHandler(async (req, res) => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  /** Check if the post is already liked by user */
+  const { data: duplicate, error: duplicateError } = await supabase
+    .from("postlikes")
+    .select()
+    .eq("likedpost", req.params.postId)
+    .eq("user_id", user.id);
+  if (duplicate.length !== 0) {
+    return res.status(400).json({ error: "You have already liked this post" });
+  }
+
+  /** Fetch the post */
+  const { data: fetchPost, error: fetchError } = await supabase
+    .from("posts")
+    .select()
+    .eq("post_id", req.params.postId);
+  if (fetchError) {
+    return res.status(400).json({ error: fetchError.message });
+  }
+
+  /** Update likes on fetched post */
+  const { error: updateError } = await supabase
+    .from("posts")
+    .update({
+      likes: fetchPost[0].likes + 1,
+    })
+    .eq("post_id", req.params.postId);
+  if (updateError) {
+    return res.status(400).json({ error: updateError.message });
+  }
+
+  /** Add the like record in postlikes table */
+  const { error: likeError } = await supabase.from("postlikes").insert({
+    likedpost: req.params.postId,
+    user_id: user.id,
+  });
+  if (likeError) {
+    return res.status(400).json({ error: likeError.message });
+  }
+
+  /** Finally, update the number of likes in user profile */
+  const { error } = await adminAuthClient.updateUserById(user.id, {
+    user_metadata: { likedposts: user.user_metadata.likedposts + 1 },
+  });
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.status(200).json({ message: "Post Liked" });
+});
+
+exports.unlikePost = asyncHandler(async (req, res) => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  /** Delete the like record in postlikes table */
+  const { error: likeError } = await supabase
+    .from("postlikes")
+    .delete()
+    .eq("likedpost", req.params.postId)
+    .eq("user_id", user.id);
+  if (likeError) {
+    return res.status(400).json({ error: likeError.message });
+  }
+
+  /** Fetch the post */
+  const { data: fetchPost, error: fetchError } = await supabase
+    .from("posts")
+    .select()
+    .eq("post_id", req.params.postId);
+  if (fetchError) {
+    return res.status(400).json({ error: fetchError.message });
+  }
+
+  /** Update likes on fetched post */
+  const { error: updateError } = await supabase
+    .from("posts")
+    .update({
+      likes: fetchPost[0].likes - 1,
+    })
+    .eq("post_id", req.params.postId);
+  if (updateError) {
+    return res.status(400).json({ error: updateError.message });
+  }
+
+  /** Finally, update the number of likes in user profile */
+  const { error } = await adminAuthClient.updateUserById(user.id, {
+    user_metadata: { likedposts: user.user_metadata.likedposts + 1 },
+  });
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.status(200).json({ message: "Post unliked" });
+});
+
+exports.getLikes = asyncHandler(async (req, res) => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  const { data: likes, error: fetchError } = await supabase
+    .from("postlikes")
+    .select()
+    .eq("likedpost", req.params.postId);
+  if (fetchError) {
+    return res.status(400).json({ error: fetchError.message });
+  }
+  res.status(200).json(likes);
 });
--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getPost, createPost, deletePost, updatePost } = require('../controllers/postController');
+const { getPost, createPost, deletePost, updatePost, getLikes, likePost, unlikePost } = require('../controllers/postController');
 const router = express.Router();
 const app = express()
 
@@ -7,5 +7,9 @@ router.get("/:postId" , getPost)
 router.post("/" , createPost)
 router.delete("/:postId" , deletePost)
 router.put("/:postId" , updatePost)
+
+router.get('/:postId/like' , getLikes)
+router.post('/:postId/like' , likePost)
+router.delete('/:postId/like' , unlikePost)
 
 module.exports = router
